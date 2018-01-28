2cd151a04170320460e2d57416af1ad4d642b948
“Seb <seb@propeller-studios.co.uk>
2018-01-28 19:32:51 +0000

    HTTP error handling

--- src/app/employees.service.ts
+++ src/app/employees.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
 import { IEmployee } from './employee';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class EmployeesService {
 
-  private _url = '/assets/data/employees.json';
+  private _url = '/assets/data/employees.jsosn';
 
   constructor(private http: HttpClient) { }
 
   getEmployees(): Observable<IEmployee[]> {
-    return this.http.get<IEmployee[]>(this._url);
+    return this.http.get<IEmployee[]>(this._url).catch(this.errorHandler);
   }
 
+  errorHandler(error: HttpErrorResponse) {
+    return Observable.throw(error.message || 'Server Error');
+  }
 }
